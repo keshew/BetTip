75b35ca42bc1d820f7a3725c1d2b802fe0a0035f
keshew <artyom.korotckov2017@yandex.ru>
2025-10-04 13:39:51 +0500

    Initial Commit

---
+++ BetTip/BetTipApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetTipApp.swift
+//  BetTip
+//
+//  Created by Артём Коротков on 04.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct BetTipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
